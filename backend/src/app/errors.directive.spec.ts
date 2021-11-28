451c2e34fce8f5382495d6b144f09de67a79a705
yoann STEPHANUS <yste>
2021-11-28 23:34:48 +0100

    suppression fichier test

--- backend/src/app/errors.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorsDirective } from './errors.directive';
-
-describe('ErrorsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ErrorsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
