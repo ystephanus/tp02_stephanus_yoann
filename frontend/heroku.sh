cdec03d58275c21a54261a64bd8d58f3840cbeee
yoann STEPHANUS <yste>
2021-11-29 23:07:16 +0100

    change title

---
+++ frontend/heroku.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dirName="tp2"
+repoName="tp02em"
+
+ng build
+cd dist/$dirName
+
+# First Use
+heroku login
+heroku create $repoName
+
+git init
+touch composer.json
+# /First Use
+
+git add .
+git commit -m "first commit"
+
+heroku git:remote -a $repoName
+
+git push heroku master #Or 'main'
